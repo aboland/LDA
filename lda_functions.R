d5dbfdca1765c4bcac312ebabae9c3497b1e5041
aboland <aidan.boland@ucd.ie>
2015-08-11 09:49:21 +0100

    Function working, able to create a corpus.

--- lda_functions.R
+++ lda_functions.R
@@ -7,11 +7,9 @@
 # https://www.cs.princeton.edu/~blei/papers/BleiNgJordan2003.pdf
 #
 
-library(MCMCpack)  # alternative library(gtools)
+library(MCMCpack)  # for dirichlet dist, alternative library(gtools)
 
-my_vocab <- c("the", "quick", "brown", "fox", "jumps", "over", "lazy", "dog")
-
-generate_lda <- function(alpha, beta, xi, Vocab, ndoc){
+generate_lda <- function(alpha, beta, xi, Vocab, Ndoc){
   # function to generate a single document
   # arguments:
   #   alpha, k x 1 concentration paramter for topics, (take input as single value for now)
@@ -24,20 +22,40 @@ generate_lda <- function(alpha, beta, xi, Vocab, ndoc){
   k <- nrow(beta)
   alpha <- rep(alpha, k)
   
-  if(dim(beta)[2]!=length(Vocab))
+  if(dim(beta)[2] != length(Vocab))
     stop("Incorrect dimension for Beta!!")
   
-  w <- array(0,c(Ndoc,N))  # blank array to store words, w_i,j will be a numeric value corresponding to a word in Vocab
-  
+  w <- list()  # blank list to store words, documents diff size so using an array is not suitable
+
   for(i in 1:Ndoc){
-    N <- rpois(xi)
-    theta <- rdirichlet(n=1, alpha=alpha)
+    N <- rpois(1, lambda = xi)  # Choose the length of the ith doc
+    theta <- rdirichlet(n=1, alpha = alpha)
+    z <- rmultinom(1, 1, prob = theta)
+    phi <- rdirichlet(n=1, alpha = beta[z,])
+    w[[i]] <- Vocab[which(rmultinom(1, 1, prob = phi) == 1)]  # Initial the ith item in the list w
+    
     for(j in 1:N){
-      z <- rmultinom(1, 1, prob=theta)
-      phi <- rdirichlet(n=1, alpha=beta[z,])
-      w[i,j] <- rmultinom(1, 1, prob=phi)
+      z <- rmultinom(1, 1, prob = theta)
+      phi <- rdirichlet(n=1, alpha = beta[z,])
+      w[[i]] <- c(w[[i]], Vocab[which(rmultinom(1, 1, prob = phi) == 1)])
     }
   }
-  
-  
+  w
 }
+
+
+ # ---------------- Generating a corpus
+
+# my_vocab <- c("the", "quick", "brown", "fox", "jumps", "over", "lazy", "dog")
+my_vocab <- c("car", "engine", "exhaust", "wheel",
+              "milk", "cream", "dairy", "yogurt",
+              "coke", "water", "juice", "coffee", "drink", "bottle", "can")
+
+n_topics <- 3
+
+my_beta <- matrix(c(1, 1, 1, 1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
+                    0.1, 0.1, 0.1, 0.1, 1, 1, 1, 1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
+                    0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 1, 1, 1, 1, 1, 1, 1), byrow = T,
+                  nrow = n_topics, ncol = length(my_vocab))
+
+my_corpus <- generate_lda(alpha = 1, beta = my_beta, xi = 3, Vocab = my_vocab, Ndoc = 6)
