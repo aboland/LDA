74d2496e60e29a06cbe4afe0f44b3b3070eda5f0
aboland <aidan.boland@ucd.ie>
2015-08-11 15:15:57 +0100

    functions working for the expectation step and for maximising beta.

--- implementation.R
+++ implementation.R
@@ -13,17 +13,30 @@ source('lda_functions.R')
 
 # ---------------- Generating a corpus
 
-# my_vocab <- c("the", "quick", "brown", "fox", "jumps", "over", "lazy", "dog")
 my_vocab <- c("car", "engine", "exhaust", "wheel",
               "milk", "cream", "dairy", "yogurt",
               "coke", "water", "juice", "coffee", "drink", "bottle", "can")
 
 n_topics <- 3
 
+ # beta matrix needs a row for each topic, and a column for each word in vocab
 my_beta <- matrix(c(10, 10, 10, 10, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
                     0.1, 0.1, 0.1, 0.1, 10, 10, 10, 10, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
                     0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 10, 10, 10, 10, 10, 10, 10), 
                   byrow = T, nrow = n_topics, ncol = length(my_vocab))
 
-my_corpus <- generate_lda(alpha = 0.1, beta = my_beta, xi = 4, Vocab = my_vocab, Ndoc = 6)
+my_corpus <- generate_lda(alpha = 0.1, beta = my_beta, xi = 5, Vocab = my_vocab, Ndoc = 20)
 my_corpus
+
+
+# ------------------ Estimating alpha and beta
+
+library(tm)
+my_corpus
+my_dtm <- as.array(DocumentTermMatrix(Corpus(VectorSource(my_corpus))))
+# need to check my_dtm, as some words may not be in any of the generated documents....
+dim(my_dtm)
+
+expect <- variational_Expectation_full(alpha=0.1, beta = my_beta, dtm = my_dtm, epsilon = 0.1)
+
+maximum <- maximization(variational_para = expect, dtm = my_dtm)
--- lda_functions.R
+++ lda_functions.R
@@ -43,3 +43,94 @@ generate_lda <- function(alpha, beta, xi, Vocab, Ndoc){
   }
   w
 }
+
+
+# This function is wrong!!!!
+# Should be seperate gamma and psi for each document!!!
+variational_Expectation <- function(alpha, beta, epsilon = 0.1){
+  # function to cary out variational inference as described in Blei (pg's 1001-1005)
+  # Expectation step of EM alg
+  # arguments:
+  #   alpha
+  #   beta
+  n_topics <- nrow(beta)
+  n_words <- ncol(beta)
+  alpha <- rep(alpha, n_topics)  # Using fixed alpha...can be easily changed
+  psi <- psi_new <- array(1/n_topics, c(n_topics, n_words))
+  gamma <- gamma_new <- alpha + (n_words/n_topics)
+  conv.test <- epsilon + 1
+
+  while(conv.test > epsilon){
+    for(n in 1:n_words)
+      for(i in 1:n_topics)
+        psi_new[i,n] <- beta[i,n] * exp(digamma(gamma[i]))  # update psi
+      psi_new <- psi_new / colSums(psi_new)  # normalise psi
+      gamma_new <- alpha + rowSums(psi_new)  # update gamma
+
+      conv.test <- max(c(abs(gamma - gamma_new),abs(psi - psi_new)))  # check for convergence
+      gamma <- gamma_new
+      psi <- psi_new
+  }
+  list(gamma, psi)
+}
+
+
+variational_Expectation_full <- function(alpha, beta, dtm, epsilon = 0.1){
+  # function to cary out variational inference as described in Blei (pg's 1001-1005)
+  # Expectation step of EM alg
+  #
+  # arguments:
+  #   alpha
+  #   beta
+  #   dtm, document term matrix
+  
+  n_topics <- nrow(beta)
+  n_docs <- nrow(dtm)
+  output <- list()
+  
+  pb <- txtProgressBar(min = 0, max = n_docs, style = 3)
+  for(d in 1:n_docs){
+    setTxtProgressBar(pb, d)
+    n_words <- sum(dtm[d,]!=0)  # number of unique words
+    alpha_temp <- rep(alpha, n_topics)  # Using fixed alpha...can be easily changed
+    beta_temp <- array(beta[,which(dtm[d,]!=0)] ,c(n_topics,n_words))
+    psi <- psi_new <- array(1/n_topics, c(n_topics, n_words))
+    gamma <- gamma_new <- alpha_temp + (n_words/n_topics)
+    conv.test <- epsilon + 1
+
+    while(conv.test > epsilon){
+      for(n in 1:n_words)
+        for(i in 1:n_topics)
+          psi_new[i,n] <- beta_temp[i,n] * exp(digamma(gamma[i]))  # update psi
+        psi_new <- psi_new / colSums(psi_new)  # normalise psi
+        gamma_new <- alpha_temp + rowSums(psi_new)  # update gamma
+      
+        conv.test <- max(c(abs(gamma - gamma_new), abs(psi - psi_new)))  # check for convergence
+        gamma <- gamma_new
+        psi <- psi_new
+    }
+    output[[d]]<-list(gamma = gamma, psi = psi)
+  }
+  close(pb)
+  output
+}
+
+
+maximization <- function(variational_para, dtm){
+  # Function to carry out maximization step of Blei
+  # arguments:
+  #   gamma and psi, the two variational parameters
+  #   dtm, document term matrix, where d,nth entry is 1 if word n is in document d (not sure about multiple words in the same doc)
+  
+  #if(length(gamma) != nrow(psi) || ncol(dtm) != ncol(psi))  # check that the dimensions match
+  #  stop("Inncorrect dimensions of parameters!")
+  n_topics <- nrow(variational_para[[1]]$psi)
+  
+  beta <- array(0, c(n_topics, ncol(dtm)))
+  for(d in 1:length(variational_para)){
+    #n_words <- ncol(variational_para[[d]]$psi)
+    for(i in 1:n_topics)
+      beta[i,which(dtm[d,]!=0)] <- beta[i,which(dtm[d,]!=0)] + variational_para[[d]]$psi[i,]  # * dtm[d,which(dtm[d,]!=0)])
+  }
+  beta
+}
