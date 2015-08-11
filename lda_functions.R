0a8e361211f234c6c4fa5ffa737ee4a2b18182b5
aboland <aidan.boland@ucd.ie>
2015-08-11 10:49:12 +0100

    Minor code change.

--- lda_functions.R
+++ lda_functions.R
@@ -32,7 +32,7 @@ generate_lda <- function(alpha, beta, xi, Vocab, Ndoc){
     theta <- rdirichlet(n=1, alpha = alpha)
     z <- rmultinom(1, 1, prob = theta)
     phi <- rdirichlet(n=1, alpha = beta[z,])
-    w[[i]] <- Vocab[which(rmultinom(1, 1, prob = phi) == 1)]  # Initial the ith item in the list w
+    w[[i]] <- Vocab[which(rmultinom(1, 1, prob = phi) == 1)]  # Initialise the ith item in the list w
     
     for(j in 1:N){
       z <- rmultinom(1, 1, prob = theta)
@@ -55,7 +55,11 @@ n_topics <- 3
 
 my_beta <- matrix(c(1, 1, 1, 1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
                     0.1, 0.1, 0.1, 0.1, 1, 1, 1, 1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
-                    0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 1, 1, 1, 1, 1, 1, 1), byrow = T,
-                  nrow = n_topics, ncol = length(my_vocab))
+                    0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 1, 1, 1, 1, 1, 1, 1), 
+                  byrow = T, nrow = n_topics, ncol = length(my_vocab))
 
 my_corpus <- generate_lda(alpha = 1, beta = my_beta, xi = 3, Vocab = my_vocab, Ndoc = 6)
+
+
+
+
